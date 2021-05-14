de578c5a682ed6ef06031afe2ddd86135ff7bfc9
Kamil <wojcikakamil@gmail.com>
2021-05-14 23:55:07 +0200

    fixes

--- src/main/java/com/kw/nodeimageeditorbackend/project/controllers/ProjectDetailsController.java
+++ src/main/java/com/kw/nodeimageeditorbackend/project/controllers/ProjectDetailsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/")
 public class ProjectDetailsController {
